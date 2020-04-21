e61a4420c6cf7ce1d45bf2dd8d3a60fc3a33e2bf
Reto Gmür <reto@factsmission.com>
2020-04-21 17:45:37 +0200

    experimenting on how to create module

---
+++ src/components.ts
@@ -0,0 +1,11 @@
+import TermEditor from '@/components/TermEditor.vue'
+import QuadEditor from '@/components/QuadEditor.vue'
+import DatasetEditor from '@/components/DatasetEditor.vue'
+import DatasetBrowser from '@/components/DatasetBrowser.vue'
+
+export {
+    TermEditor,
+    QuadEditor,
+    DatasetEditor,
+    DatasetBrowser
+}
