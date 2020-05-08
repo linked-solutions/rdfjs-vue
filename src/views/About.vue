bfbfb5369bc2fbeece8c84709945eaaf5dc99acb
Reto Gmür <reto@factsmission.com>
2020-05-08 15:20:24 +0200

    not liked

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,6 +4,5 @@
     <p>RDFJS Vue has been coded* by Noam and Reto, check
       out <a href="https://factsmission.com/">factsmission.com</a> 
       for more of our stuff.</p>
-    <p>* With ❤️ obviously, dead people don't code.</p>
   </div>
 </template>
